d4a72cc78827c10d0c2249e58b027e84650fd57a
M4c13j <maciekcx11@gmail.com>
2024-07-08 22:28:02 +0200

    rest of chnages...

--- build/CMakeFiles/RaylibImGuiExample.dir/DependInfo.cmake
+++ build/CMakeFiles/RaylibImGuiExample.dir/DependInfo.cmake
@@ -8,12 +8,15 @@ set(CMAKE_DEPENDS_LANGUAGES
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "/home/mac/gitrepos/rayilb-zabawa/main.cpp" "CMakeFiles/RaylibImGuiExample.dir/main.cpp.o" "gcc" "CMakeFiles/RaylibImGuiExample.dir/main.cpp.o.d"
+  "/Users/maciej/Desktop/GitRepos/raylib-nauka/main.cpp" "CMakeFiles/RaylibImGuiExample.dir/main.cpp.o" "gcc" "CMakeFiles/RaylibImGuiExample.dir/main.cpp.o.d"
   )
 
-# Targets to which this target links.
-set(CMAKE_TARGET_LINKED_INFO_FILES
-  "/home/mac/gitrepos/rayilb-zabawa/build/CMakeFiles/imgui.dir/DependInfo.cmake"
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
   )
 
 # Fortran module output directory.
