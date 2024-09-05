3299b1041436194d69ee1f4664efd9fb26f49833
mcuper <maciekcx11@gmail.com>
2024-09-05 18:29:38 +0200

    Showcase and changed debug/demo options.

--- src/config.hpp
+++ src/config.hpp
@@ -7,7 +7,7 @@ constexpr int BLOCK_HEIGHT = 16;
 
 constexpr int CHUNK_SIZE           = 16; // chunks are cubes
 constexpr int BLOCKS_IN_CHUNK      = CHUNK_SIZE * CHUNK_SIZE * CHUNK_SIZE;
-constexpr int MAP_SIDE_IN_CHUNKS   = 16; // assuming square map
+constexpr int MAP_SIDE_IN_CHUNKS   = 20; // assuming square map
 constexpr int MAP_HEIGHT_IN_CHUNKS = 6;
 constexpr int MAP_SIDE_IN_BLOCKS   = MAP_SIDE_IN_CHUNKS * CHUNK_SIZE;
 constexpr int MAP_HEIGHT_IN_BLOCKS = MAP_HEIGHT_IN_CHUNKS * CHUNK_SIZE;
--- src/main.cpp
+++ src/main.cpp
@@ -21,7 +21,7 @@
 int main(int argc, char** argv)
 {
     // Initialize raylib ======================
-    InitWindow(1200, 700, "Raylib + ImGui Example");
+    InitWindow(1600, 900, "Raylib + ImGui Example");
     SetTargetFPS(60);
     SetWindowState(FLAG_WINDOW_RESIZABLE ); //| FLAG_BORDERLESS_WINDOWED_MODE);
     SetConfigFlags(FLAG_MSAA_4X_HINT | FLAG_VSYNC_HINT); // Anti-aliasing and V-Sync
@@ -34,14 +34,14 @@ int main(int argc, char** argv)
 
     Chunk chunk = Chunk({0, -config::CHUNK_SIZE/2,0}, 0);
     // chunk.generate_default_blocks(config::CHUNK_HEIGHT / 2);
-    chunk.generate_perlin(2137u);
+    chunk.generate_perlin(1241241);
     // chunk.generate_mesh();
 
     World *world = new World(); // stack overfow for bigger worlds if allocated on stack;
     Benchmark bench("Chunk mesh ", 1);
     Benchmark single("Single chunk mesh, with neighbours", 1);
     bench.start();
-        world->generate_perlin_chunks(10000000u);
+        world->generate_perlin_chunks(123123);
         world->mesh_all_chunks();
     bench.stop(world->side * world->side * world->height);
     single.start();
@@ -66,28 +66,38 @@ int main(int argc, char** argv)
 
         // FLUID DEMO ==================================================================================================
         // if (GetTime() > startTimefl && GetTime() < stopTimefl && DAWG) {
-        //     for (int x = -4; x <= 4; x++) {
-        //         for (int z = 6; z <= 10; z++) {
-        //             world->fluidSim.addBlock(x, 15, z);
-        //         }
-        //     }
-        //     // world->addFluid(-4, 20, 9);
-        //     startTimefl += 0.1;
-        //     // DAWG = false;
-        // }
-
-        // SAND DEMO =================================================
-        if (GetTime() > startTimesa && GetTime() < stopTimesa && DAWG) {
+        if (IsKeyDown(KEY_N)) {
             for (int x = -4; x <= 4; x++) {
                 for (int z = 6; z <= 10; z++) {
-                    world->sandSim.addBlock(x-10, 15-10, z-10);
-                    world->sandSim.addBlock(x-10, 15-10+1, z-10);
+                    world->fluidSim.addBlock(x-23, 47, z-21);
                 }
             }
-            world->sandSim.addBlock(-15-10, 30, -15-10);
-            // world->sandSim.addBlock(-15-10, 15-10+2, -15-10);
-            // world->sandSim.addBlock(-15-10, 15-10+1, -15-10);
+            startTimefl += 0.1;
+            // DAWG = false;
+        }
+
+        // SAND DEMO =================================================
+        // if (GetTime() > startTimesa && GetTime() < stopTimesa && DAWG) {
+        if (IsKeyDown(KEY_B)) {
+            for (int x = -2; x <= 1; x++) {
+                for (int z = 6; z <= 8; z++) {
+                    world->sandSim.addBlock(x - 10, 15 + 30, z - 10);
+                    world->sandSim.addBlock(x - 10, 15 - 30 + 1, z - 10);
+                }
+            }
+            world->sandSim.addBlock(-15 - 10, 30, -15 - 10);
             startTimesa += 0.1f;
+        }
+
+        // BELOW PLAYER ================================================
+        Cord currPos{static_cast<int>(player.camera.position.x), static_cast<int>(player.camera.position.y),
+                  static_cast<int>(player.camera.position.z)};
+
+        if (IsKeyDown(KEY_V)) {
+            world->sandSim.addBlock(currPos);
+        }
+        if (IsKeyDown(KEY_M)) {
+            world->fluidSim.addBlock(currPos);
         }
 
         // UPDATE WORLD AND SIMULATION =================================================================================
@@ -109,11 +119,11 @@ int main(int argc, char** argv)
         ClearBackground(RAYWHITE);
 
             BeginMode3D(player.camera);
-                DrawCubeWiresV((Vector3){ 0.0f, 0.5f, 1.0f }, (Vector3){ 1.0f, 1.0f, 1.0f }, RED);
-                DrawCubeV((Vector3){ 0.0f, 0.5f, 1.0f }, (Vector3){ 1.0f, 1.0f, 1.0f }, PURPLE);
-                DrawCubeWiresV((Vector3){ 0.0f, 0.5f, -1.0f }, (Vector3){ 1.0f, 1.0f, 1.0f }, DARKGREEN);
-                DrawCubeV((Vector3) { 0.0f, 0.5f, -1.0f }, (Vector3){ 1.0f, 3.0f, 1.0f }, YELLOW);
-                DrawGrid(100, 1.0f);
+                // DrawCubeWiresV((Vector3){ 0.0f, 0.5f, 1.0f }, (Vector3){ 1.0f, 1.0f, 1.0f }, RED);
+                // DrawCubeV((Vector3){ 0.0f, 0.5f, 1.0f }, (Vector3){ 1.0f, 1.0f, 1.0f }, PURPLE);
+                // DrawCubeWiresV((Vector3){ 0.0f, 0.5f, -1.0f }, (Vector3){ 1.0f, 1.0f, 1.0f }, DARKGREEN);
+                // DrawCubeV((Vector3) { 0.0f, 0.5f, -1.0f }, (Vector3){ 1.0f, 3.0f, 1.0f }, YELLOW);
+                // DrawGrid(100, 1.0f);
 
                 world->draw_all(dirt_plank, DRAW_CHUNK_DEBUG_WIRES_MODEL);
                 chunk.draw_chunk(dirt_plank, DRAW_CHUNK_DEBUG_WIRES_MODEL);
@@ -123,13 +133,13 @@ int main(int argc, char** argv)
                 // DrawBoundingBox(GetMeshBoundingBox(chunk.chunkMesh), BLACK);
             EndMode3D();
 
-             // debug stays
-             DrawRectangleGradientV(0, 0, 350, 120,{246, 233, 107, 255}, {56, 127, 57, 255});
-             DrawText("This is a raylib window with ImGui!", 10, 10, 20, BLACK);
+             // debug status====================================================================================================
+             DrawRectangleGradientV(0, 0, 350, 120,BLUE, RED);
+             DrawText("SandVoxel debug.", 10, 10, 20, BLACK);
              DrawText(TextFormat("Fps: %d  |  Frame time: %.2fms", GetFPS(), GetFrameTime()*1000), 10, 30, 20, BLACK);
-             DrawText(TextFormat("  Chunk: %s", world->chunk_cord_of_block(player.camera.position).toString().c_str()), 10, 50, 20, BLACK);
-             DrawText(TextFormat("Camera Position: [%.2f, %.2f, %.2f]", player.camera.position.x, player.camera.position.y, player.camera.position.z), 10, 70, 20, BLACK);
-             DrawText(TextFormat("  Camera target: [%.2f, %.2f, %.2f]", player.camera.target.x, player.camera.target.y, player.camera.target.z), 10, 90, 20, BLACK);
+             DrawText(TextFormat("Chunk: %s", world->chunk_cord_of_block(player.camera.position).toString().c_str()), 10, 50, 20, BLACK);
+             DrawText(TextFormat("pos: [%.2f, %.2f, %.2f]", player.camera.position.x, player.camera.position.y, player.camera.position.z), 10, 70, 20, BLACK);
+             DrawText(TextFormat("target: [%.2f, %.2f, %.2f]", player.camera.target.x, player.camera.target.y, player.camera.target.z), 10, 90, 20, BLACK);
 
          EndDrawing();
     }
